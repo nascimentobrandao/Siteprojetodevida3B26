b2eefa3b6f4b7a4b5f0b58e4f543b9a4fe682833
nascimentobrandao <nascimento.brandao@escola.pr.gov.br>
2026-06-10 12:10:58 -0300

    asdfghjk

--- main.js
+++ main.js
@@ -24,4 +24,11 @@ function calculaTempo(tempoObjetivo){
     let tempoFinal = tempoObjetivo - tempoAtual;
     let segundos = Math.floor(tempoFinal/1000);
     let minutos = Math.floor(segundos/60);
+    let horas = Math.floor (minutos/60);
+    let dias = Math.floor (horas/24);
+
+segundos %= 60;
+minutos %= 60;
+horas %= 24;
+return dias + " dias " + horas " horas " + minutos + " minutos " + segundos + " segundos ";
 }
